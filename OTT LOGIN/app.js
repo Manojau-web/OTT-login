d33be446cd16a7c229154fb3d381953061fd396c
Manojau-web <65848418+Manojau-web@users.noreply.github.com>
2024-01-04 02:57:26 +0530

    Add files via upload

---
+++ OTT LOGIN/app.js
@@ -0,0 +1,17 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAOMNL0ot8WDqknxTcDKubXXYJZWt1m7eA",
+    authDomain: "sign-up-login-project.firebaseapp.com",
+    projectId: "sign-up-login-project",
+    storageBucket: "sign-up-login-project.appspot.com",
+    messagingSenderId: "145462676143",
+    appId: "1:145462676143:web:6a5977f5f1753f6607625a"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
